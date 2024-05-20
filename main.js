14f017818d3a4da39175e9809721b26c1951f9c2
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 03:32:01 +0530

    done2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
-writeFile('message.txt', "Hello, World!", (err) => {
+fs.writeFile('output.txt', "Hello, World!", (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   }); 
